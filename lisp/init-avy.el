58c654970b201a982987623269282836b91f425f
Sergey Naumov <drmgc@yandex.ru>
2020-10-07 17:04:32 +0300

    init

---
+++ lisp/init-avy.el
@@ -0,0 +1,10 @@
+;;; init-avy.el --- avy navigation -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'avy)
+(global-set-key (kbd "C-c j") 'avy-goto-word-or-subword-1)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+
+(provide 'init-avy)
+;;; init-avy.el ends here
