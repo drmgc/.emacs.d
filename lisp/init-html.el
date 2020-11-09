4ffc088f96f066dd7aac394cb91563a1c0640e18
Sergey Naumov <drmgc@yandex.ru>
2020-11-09 17:52:11 +0300

    html: web-mode

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -5,15 +5,55 @@
 
 ;;; Code:
 
-(require-package 'tagedit)
-(with-eval-after-load 'sgml-mode
-  (tagedit-add-paredit-like-keybindings)
-  (define-key tagedit-mode-map (kbd "M-?") nil)
-  (define-key tagedit-mode-map (kbd "M-s") nil)
-  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
+;; (require-package 'tagedit)
+;; (with-eval-after-load 'sgml-mode
+;;   (tagedit-add-paredit-like-keybindings)
+;;   (define-key tagedit-mode-map (kbd "M-?") nil)
+;;   (define-key tagedit-mode-map (kbd "M-s") nil)
+;;   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
 (add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
 
+(require-package 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.j2\\'" . web-mode))
+
+(add-hook 'web-mode-hook
+	  (lambda ()
+	    (set (make-local-variable 'company-backends)
+		 '(company-css
+		   company-web-html
+		   company-files
+		   company-yasnippet))
+	    (setq tab-width 4
+		  indent-tabs-mode t
+		  web-mode-markup-indent-offset 4
+		  web-mode-code-indent-offset 4
+		  web-mode-css-indent-offset 4)
+            (web-mode-use-tabs)
+	    (setq web-mode-enable-current-column-hightlight t
+		  web-mode-enable-current-element-hightlight t)
+            (setq web-mode-enable-auto-pairing nil)
+            (setq web-mode-engine-alist
+                  '(("django" . "\\.j2\\'")))))
+
+(require-package 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'emmet-mode-hook
+          (lambda ()
+            (setq emmet-indentation 4)))
+
+;; (add-hook 'html-mode ())
+;; (when (maybe-require-package 'multi-web-mode)
+;;   (setq mweb-default-major-mode 'html-mode)
+;;   (setq mweb-tags
+;;	'((js-mode "<script[^>]*>" "</script>")
+;;	  (css-mode "<style[^>]*>" "</style>")))
+;;   (setq 'mweb-filename-extensions '("html" "htm"))
+;;   )
 
 (provide 'init-html)
 ;;; init-html.el ends here
