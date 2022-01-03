c0bc0ea475d4c1d3889ee119871ba8859e96868d
Sergey Naumov <drmgc@yandex.ru>
2022-01-03 17:14:36 +0300

    yaml

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'yaml-mode)
+(when (require-package 'yaml-mode)
   (add-auto-mode 'yaml-mode "\\.yml\\.erb\\'")
   (add-hook 'yaml-mode-hook 'goto-address-prog-mode))
 
