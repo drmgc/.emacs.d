cab9e963f0ef26759f40aa797731a3d9e69e9b57
Sergey Naumov <drmgc@yandex.ru>
2022-03-01 17:38:31 +0300

    display startup time

--- init.el
+++ init.el
@@ -2,7 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun efs/display-startup-time ()
+  (message
+   "Emacs loaded in %s with %d garbage collections."
+   (format
+    "%.2f seconds"
+    (float-time
+     (time-subtract after-init-time before-init-time)))
+   gcs-done))
 
+(add-hook 'emacs-startup-hook #'efs/display-startup-time)
 ;;(setq debug-on-error t)
 
 ;; Make frame transparency overridable
